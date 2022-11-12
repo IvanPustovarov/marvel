77a792ce19abaf34da9108b22d019cafdb16925a
LinderVike <lindervike2018@gmail.com>
2022-11-12 22:55:30 +0800

    commit message

--- src/store/index.js
+++ src/store/index.js
@@ -9,10 +9,6 @@ export default createStore({
         description: "Вася пупкин",
       },
       {
-        name: "absorbing-man",
-        description: "On Reveal: If the last card you played has an On Reveal ability, this card copies it.",
-      },
-      {
         name: "adam-warlock",
         description: "ongoing my anime",
       },
