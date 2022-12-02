524e63c6372d3e4036f906ce73ccc32ace5104c6
John <silverflash150@gmail.com>
2022-12-02 23:27:10 +0300

    adding dist subtree

---
+++ dist/js/381.74bb5fd2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmarvel"]=self["webpackChunkmarvel"]||[]).push([[381],{7381:function(n,a,e){e.r(a),e.d(a,{default:function(){return f}});var r=e(3396);const u={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),s=[t];function c(n,a){return(0,r.wg)(),(0,r.iD)("div",u,s)}var i=e(89);const l={},o=(0,i.Z)(l,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=381.74bb5fd2.js.map
